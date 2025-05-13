b6fb646ff5698d11196f736222f7d93c6d1797bb
Jeki <ahmadzaqi490@gmail.com>
2025-05-13 15:46:39 +0700

    Update remote

--- app/src/main/java/com/takumi/kasirkain/data/remote/ApiService.kt
+++ app/src/main/java/com/takumi/kasirkain/data/remote/ApiService.kt
@@ -4,11 +4,15 @@ import com.takumi.kasirkain.data.remote.request.LoginRequest
 import com.takumi.kasirkain.data.remote.response.BaseResponse
 import com.takumi.kasirkain.data.remote.response.CategoryResponse
 import com.takumi.kasirkain.data.remote.response.LoginResponse
+import com.takumi.kasirkain.data.remote.response.ProductDetailResponse
 import com.takumi.kasirkain.data.remote.response.ProductResponse
+import com.takumi.kasirkain.data.remote.response.ProductVariantResponse
+import com.takumi.kasirkain.data.remote.response.TransactionResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -25,4 +29,12 @@ interface ApiService {
 
     @GET("categories")
     suspend fun getCategories(): Response<BaseResponse<List<CategoryResponse>>>
+
+    @GET("product-variant/detail/{barcode}")
+    suspend fun getProductVariantDetail(
+        @Path("barcode") barcode: String
+    ): Response<BaseResponse<ProductDetailResponse>>
+
+    @GET("transactions")
+    suspend fun getTransactions(): Response<BaseResponse<List<TransactionResponse>>>
 }
